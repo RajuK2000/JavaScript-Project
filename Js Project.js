551c68e936dfb60f409ced6aa16ce56ea276707d
RajukumarK2000 <kasarapurajukumar04@gmail.com>
2024-03-29 23:50:06 +0530

    UI Changes

--- Js Project.js
+++ Js Project.js
@@ -1,34 +1,34 @@
 
 
-var di=document.getElementById("di11")
+    var di=document.getElementById("di11")
 
-            document.forms[0].addEventListener("submit",(e)=>{
+      document.forms[0].addEventListener("submit",(e)=>{
                di.innerHTML=""
               e.preventDefault()
-             
-         fetch(`https://indian-zipcodes.p.rapidapi.com/api/data?pincode=${e.target[0].value}`,
+      fetch(`https://india-pincode-with-latitude-and-longitude.p.rapidapi.com/api/v1/pincode/${e.target[0].value}`,
           { 
-              methode:"GET",
-                headers:{
-              'X-RapidAPI-Key': '85c39f570bmsh6021967c3278783p1143c2jsnae41703e9b6f',
-		      'X-RapidAPI-Host': 'indian-zipcodes.p.rapidapi.com'
-         }
-     })
-     .then((zip)=>{
-         return zip.json()  
-      })
-       .then((zip)=>{
-         console.log(zip)
-         e.target[0].value=""
-          localStorage.setItem("ZIPcodes",JSON.stringify(zip))
-      
-        var zip=JSON.parse(localStorage.getItem("ZIPcodes"))
+            methode:"GET",
+            headers:{
+           'X-RapidAPI-Key': '85c39f570bmsh6021967c3278783p1143c2jsnae41703e9b6f',
+		       'X-RapidAPI-Host': 'india-pincode-with-latitude-and-longitude.p.rapidapi.com'
+                   }
+           })
+             .then((zip)=>{
+                 return zip.json()  
+              })
+                .then((zip)=>{
+                  console.log(zip)
+                  e.target[0].value=""
 
+                  localStorage.setItem("ZIPcodes",JSON.stringify(zip))
+             
+        //var zip=JSON.parse(localStorage.getItem("ZIPcodes"))
+         
         var con=document.createElement("div")
         con.setAttribute("id","tdiv")
         di.appendChild(con)
         var tab=document.createElement("table")
-        tab.setAttribute("id","tabl")
+        tab.setAttribute("class","table table-dark table-striped")
         con.appendChild(tab)
         var tr1=document.createElement("tr")
         tab.appendChild(tr1)
@@ -36,11 +36,11 @@ var di=document.getElementById("di11")
         var td1=document.createElement("td")
         td1.innerText="S.NO"
         var td2=document.createElement("td")
-        td2.innerText="District"
+        td2.innerText="Area"
         var td3=document.createElement("td")
         td3.innerText="Pincode"
         var td4=document.createElement("td")
-        td4.innerText="Taluka"
+        td4.innerText="District"
         var td5=document.createElement("td")
         td5.innerText="State"
 
@@ -50,18 +50,18 @@ var di=document.getElementById("di11")
         tr1.appendChild(td4)
         tr1.appendChild(td5)
 
-      for(i=0;i<=zip.length;i++){
+       for(i=0;i<=zip.length;i++){
         var tr2=document.createElement("tr")
         tab.appendChild(tr2)
         
         var td11=document.createElement("td")
         td11.innerText=[i+1]
         var td12=document.createElement("td")
-        td12.innerText=zip[i].district
+        td12.innerText=zip[i].area
         var td13=document.createElement("td")
         td13.innerText=zip[i].pincode
         var td14=document.createElement("td")
-        td14.innerText=zip[i].taluka
+        td14.innerText=zip[i].district
         var td15=document.createElement("td")
         td15.innerText=zip[i].state
 
@@ -70,7 +70,7 @@ var di=document.getElementById("di11")
         tr2.appendChild(td13)
         tr2.appendChild(td14)
         tr2.appendChild(td15)
-     }
-    })   
+       }
+           }) 
     
-  })
+        })
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # JavaScript-Project
+   In this project do purpuse to search a pincode areas easyway in this we enter zipcode a it shows details of areas along that pin.
+
+   it is useful for some people who now pincode but doesn't know the perfect area.
+
+   In this i am basic use to collect data usesing API
